3929821f63b18d32adb9817bf156065c9143d59b
kitakou0313 <kitakou0313@gmail.com>
2021-01-15 02:54:37 +0000

    Add 210 point ans

--- 2_14/2_14.py
+++ 2_14/2_14.py
@@ -1,16 +1,42 @@
+def getNthBit(bit, N):
+    return (bit >> N) & 1
+
+
+def convertBitToBuildings(combinationBit, buildings):
+    N = len(buildings) - 1
+    res = []
+
+    for n in range(N):
+        if getNthBit(combinationBit, n) == 1:
+            res.append(buildings[n + 1])
+
+    return res
+
+
+def calCostWithBuildingsFromBit(buildings, buildingsFromCombBit):
+    minHeight = buildings[0]
+    ans = 0
+    for i in range(len(buildingsFromCombBit)):
+        if buildingsFromCombBit[i] <= minHeight + 1:
+            ans += (minHeight + 1) - buildingsFromCombBit[i]
+            minHeight += 1
+        else:
+            minHeight = buildingsFromCombBit[i]
+    return ans
+
 
 if __name__ == "__main__":
     N, K = map(int, input().split())
     buildings = list(map(int, input().split()))
 
-    minHeight = buildings[0]
-    ans = 0
-    for i in range(1, len(buildings)):
-        if buildings[i] < minHeight + 1:
-            ans += minHeight + 1 - buildings[i]
-            minHeight += 1
-        else:
-            ans += 0
-            minHeight = buildings[i]
+    minCost = float('inf')
+    for combinationBit in range(2 ** (N-1)):
+        buildingsFromCombBit = convertBitToBuildings(combinationBit, buildings)
+        if len(buildingsFromCombBit) != (K-1):
+            continue
 
-    print(ans)
+        minCost = min(minCost, calCostWithBuildingsFromBit(
+            buildings, buildingsFromCombBit))
+        print(type(minCost))
+
+    print(minCost)
