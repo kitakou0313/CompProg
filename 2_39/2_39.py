0849654185212120fbfda21c1c295f512847fca5
kitakou0313 <kitakou0313@gmail.com>
2021-02-06 14:38:38 +0000

    Add ans of 2_39

--- 2_39/2_39.py
+++ 2_39/2_39.py
@@ -1,22 +1,25 @@
 def calNumOfEqus(nums, trgSum):
     # dp[0~20の数] = 到達できる式が何パターンあるか
     # 全部使わないのも入るのでだめでした…
-    dp = [0 for _ in range(21)]
 
-    # 3 2 1 4 5 
-    dp[nums[0]] = 1
-    for ind in range(1, len(nums)):
-        for numInDP in range(1, 21):
-            if dp[numInDP] != 0:
-                resOfSum = numInDP + nums[ind]
+    dp = [[0 for _ in range(21)] for _1 in range(len(nums))]
+
+    # 3 2 1 4 5
+
+    dp[0][nums[0]] = 1
+    for ind in range(0, len(nums)-1):
+        for numInDP in range(0, 21):
+            if dp[ind][numInDP] != 0:
+                resOfSum = numInDP + nums[ind+1]
                 if 0 <= resOfSum <= 20:
-                    dp[resOfSum] += dp[numInDP]
+                    dp[ind+1][resOfSum] += dp[ind][numInDP]
 
-                resOfSub = numInDP - nums[ind]
+                resOfSub = numInDP - nums[ind+1]
                 if 0 <= resOfSub <= 20:
-                    dp[resOfSub] += dp[numInDP]
+                    dp[ind+1][resOfSub] += dp[ind][numInDP]
 
-    return dp[trgSum]
+    return dp[len(nums)-1][trgSum]
+
 
 if __name__ == "__main__":
     N = int(input())
@@ -26,6 +29,3 @@ if __name__ == "__main__":
     nums = inputNums[:len(inputNums) - 1]
 
     print(calNumOfEqus(nums, trgSum))
-    
-
-    
