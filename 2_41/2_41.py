5f69b0f86f430d180f4723d3e8490873763984aa
kitakou0313 <kitakou0313@gmail.com>
2021-02-08 15:36:40 +0000

    Add input proc

--- 2_41/2_41.py
+++ 2_41/2_41.py
@@ -1,1 +1,18 @@
+class Cloth():
+    def __init__(self, A, B, C):
+        self.minTemp = A
+        self.maxTemp = B
+        self.showiness = C
+
+
 if __name__ == "__main__":
+    D, N = map(int, input().split())
+
+    dateAndTemp = {}
+    for d in range(D):
+        dateAndTemp[d] = int(input())
+    
+    cloths = []
+    for n in range(N):
+        A, B, C = map(int, input().split())
+        cloths.append(Cloth(A,B,C))
