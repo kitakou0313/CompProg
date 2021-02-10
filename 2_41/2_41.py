df5c22282f88f5826d64641fb5d855a591715058
kitakou0313 <kitakou0313@gmail.com>
2021-02-10 07:17:06 +0000

    Add ans of 2_41

--- 2_41/2_41.py
+++ 2_41/2_41.py
@@ -9,8 +9,8 @@ def calMaxShowiness(clothes, dateAndTemp):
     D = len(dateAndTemp)
     N = len(clothes)
 
-    # dp dp[i(日にち)][j(選んだ服)] = [i日目までの最大値、i日目に選んだ服の目立ち具合]
-    dp = [[[0, 0] for _ in range(N)] for _ in range(D)]
+    # dp dp[i(日にち)][j(選んだ服)] = i日目までの最大値
+    dp = [[0 for _ in range(N)] for _ in range(D)]
 
     for d in range(D):
         t = dateAndTemp[d]
@@ -19,29 +19,30 @@ def calMaxShowiness(clothes, dateAndTemp):
 
             # 温度で服着れない場合
             if not(clothOnJ_0.minTemp <= t <= clothOnJ_0.maxTemp):
-                dp[d][j_0] = [-1, clothOnJ_0.showiness]
+                dp[d][j_0] = -1
             else:
                 # 初日
                 if d == 0:
-                    dp[d][j_0] = [0, clothOnJ_0.showiness]
+                    dp[d][j_0] = 0
                 else:
                     maxShownessSumWithJ_0 = -1
                     # j_1 前日の服
                     for j_1 in range(N):
-                        if dp[d-1][j_1][0] == -1:
+                        if dp[d-1][j_1] == -1:
                             continue
                         else:
-                            posValue = dp[d-1][j_1][0] + \
-                                abs(clothOnJ_0.showiness - dp[d-1][j_1][1])
+                            clothOnJ_1 = clothes[j_1]
+                            posValue = dp[d-1][j_1] + \
+                                abs(clothOnJ_0.showiness - clothOnJ_1.showiness)
                             if posValue > maxShownessSumWithJ_0:
                                 maxShownessSumWithJ_0 = posValue
-                                dp[d][j_0] = [posValue, clothOnJ_0.showiness]
+                                dp[d][j_0] = posValue
 
     res = -1
 
     for n in range(N):
-        if dp[D-1][n][0] > res:
-            res = dp[D-1][n][0]
+        if dp[D-1][n] > res:
+            res = dp[D-1][n]
 
     return res
 
