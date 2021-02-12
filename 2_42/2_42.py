554e56557f83b7c689957f184bb1121ba0bfdb93
kitakou0313 <kitakou0313@gmail.com>
2021-02-12 14:51:24 +0000

    Add ans of 2_42

--- 2_42/2_42.py
+++ 2_42/2_42.py
@@ -1,29 +1,30 @@
 def calMinTukare(distanceBtwCties, dayAndTemp):
 
     N = len(distanceBtwCties) + 1
-    M = len(dayAndTemp)
+    M = len(dayAndTemp)+1
 
-    # dp[i][j] = 都市i‐1~iに日にちjに移動するときの最小の疲労度（不可能な場合は-1）
-    dp = [[0 for _ in range(N)] for _ in range(M)]
+    # dp[i][j] = 都市i‐1~iに日にちjに移動するときの最小の疲労度
+    dp = [[float("inf") for _ in range(M)] for _ in range(N)]
+
+    dp[0] = [0 for _ in range(M)]
 
     for n in range(1, N):
-        for day in range(M):
+        for day in range(1, M):
+            dp[n][day] = min(dp[n][day-1], dp[n-1][day-1] +
+                             distanceBtwCties[n]*dayAndTemp[day])
 
+    return dp[-1][-1]
 
-    return ans
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
 
-    distanceBtwCties = []
-    for n in range(N):
-        distance = int(input())
-        distanceBtwCties.append(distance)
-    
-    dayAndTemp = []
-    for m in range(M):
-        temp = int(input())
-        dayAndTemp.append(temp)
+    distanceBtwCties = {}
+    for n in range(1, N+1):
+        distanceBtwCties[n] = int(input())
+
+    dayAndTemp = {}
+    for m in range(1, M+1):
+        dayAndTemp[m] = int(input())
 
     print(calMinTukare(distanceBtwCties, dayAndTemp))
-    
