30c45b5877cf488fcbca28f38d2620b42f12c017
kitakou0313 <kitakou0313@gmail.com>
2021-02-13 15:57:35 +0000

    Add input Procedure

--- 2_43/2_43.py
+++ 2_43/2_43.py
@@ -1,1 +1,13 @@
+def calMinColorChangeNum(flags):
+    return 0
+
 if __name__ == "__main__":
+
+    N = int(input())    
+
+    flags = []
+
+    for col in range(5):
+        flags.append(input())
+
+    print(calMinColorChangeNum(flags))
