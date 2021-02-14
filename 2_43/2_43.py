884e4763552b68837192b321892f89bf4cf00b98
kitakou0313 <kitakou0313@gmail.com>
2021-02-14 08:37:30 +0000

    Add bubunnten ans

--- 2_43/2_43.py
+++ 2_43/2_43.py
@@ -1,13 +1,32 @@
+# 制約4まで
 def calMinColorChangeNum(flags):
-    return 0
+    N = len(flags[0])
+
+    colorAndRecoloredTile = {color: 5 for color in ["R", "W", "B"]}
+
+    for tile in ((flags)):
+        if tile[0] == "#":
+            continue
+        colorAndRecoloredTile[tile[0]] -= 1
+
+    colorAndRecoloredTile = sorted(colorAndRecoloredTile.items(), key=lambda x:x[1])
+
+    minColor = colorAndRecoloredTile[0]
+    secondColor = colorAndRecoloredTile[1]
+
+    if N == 1:
+        return minColor[1]
+
+    return minColor[1]*(N // 2) + secondColor[1]*(N // 2) +minColor[1]*(N % 2)
+
 
 if __name__ == "__main__":
 
-    N = int(input())    
+    N = int(input())
 
     flags = []
 
-    for col in range(5):
+    for row in range(5):
         flags.append(input())
 
     print(calMinColorChangeNum(flags))
