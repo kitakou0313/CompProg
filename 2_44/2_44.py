79c051e55699877a1f5b9e5218d92926cdbb2262
kitakou0313 <kitakou0313@gmail.com>
2021-02-16 03:58:11 +0000

    Make input procedure and ans func

--- 2_44/2_44.py
+++ 2_44/2_44.py
@@ -1,1 +1,19 @@
+def calMinPollockNumOfN(N):
+    return
+
+
+def calMinPollockNumOfNWithOddPollock(N):
+    return
+
+
 if __name__ == "__main__":
+    anses = []
+    while True:
+        N = int(input())
+        if N == 0:
+            for ans in anses:
+                print(ans[0], ans[1])
+            break
+
+        anses.append((calMinPollockNumOfN(
+            N), calMinPollockNumOfNWithOddPollock(N)))
