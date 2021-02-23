169e89dfe73c7ab5400d7d3093b6acf7d7b2771c
kitakou0313 <kitakou0313@gmail.com>
2021-02-23 14:11:52 +0000

    Add wa-syaru huroido

--- 2_60/2_60.py
+++ 2_60/2_60.py
@@ -1,10 +1,14 @@
 def calAllPairsShortestPath(V, E, edges):
-    return [[]]
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                edges[i][j] = min(edges[i][j], edges[i][k] + edges[k][j])
 
 if __name__ == "__main__":
     V, E = map(int,input().split())
 
-    edges = [[None for _ in range(V)] for _ in range(V) ]
+    #未接続時はINF, 等しい時は0
+    edges = [[float("inf") if a != b else 0 for b in range(V)] for a in range(V) ]
     for e in range(E):
         s, t, w = map(int, input().split())
         edges[s][t] = w
